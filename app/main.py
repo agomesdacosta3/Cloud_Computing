be8eb8a54dbf47449dca06fa8f14b14f7f3c9fbe
agomesdacosta3 <caps92@live.fr>
2024-01-11 17:03:07 +0100

    replacement of Postgre by Elastic

--- app/main.py
+++ app/main.py
@@ -1,32 +1,55 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, HTTPException
 
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine, text
+# from sqlalchemy.orm import sessionmaker
 
+from elasticsearch import Elasticsearch
 import requests
 
 app = FastAPI()
-engine = create_engine('postgresql://postgres:postgres@db/northwind')
-Session = sessionmaker(bind=engine)
+es = Elasticsearch('http://elastic:9200') # Connect to Elasticsearch container
 templates = Jinja2Templates(directory="templates")
 
 # Utilisez la fonction app.mount() pour servir les fichiers statiques depuis le répertoire "styles"
 app.mount("/styles", StaticFiles(directory="styles"), name="styles")
 
-# New endpoint to test Elasticsearch container
+# Endpoint to test Elasticsearch container
 @app.get('/test_elasticsearch')
 async def test_elasticsearch(request: Request):
-    # Make a request to Elasticsearch container
     try:
-        es_response = requests.get('http://127.0.0.1:9200')
-        es_content = es_response.content.decode('utf-8')
-        return templates.TemplateResponse("test_elasticsearch.html", {"request": request, "elasticsearch_response": es_content})
-    except requests.RequestException as e:
-        return templates.TemplateResponse("test_elasticsearch.html", {"request": request, "error": f"Error connecting to Elasticsearch: {str(e)}"})
+        # Documents to insert in the Elasticsearch index "cities"
+        doc1 = {"city":"New Delhi", "country":"India"}
+        doc2 = {"city":"London", "country":"England"}
+        doc3 = {"city":"Los Angeles", "country":"USA"}
+
+        #Inserting doc1 in id=1
+        es.index(index="cities", id=1, body=doc1)
+
+        #Inserting doc2 in id=2
+        es.index(index="cities", id=2, body=doc2)
+
+        #Inserting doc3 in id=3
+        es.index(index="cities", id=3, body=doc3)
+
+        # Retrieving data for id=1
+        es_response_1 = es.get(index="cities", id=1)
+        es_data_1 = es_response_1.get("_source", {})
+
+        # Retrieving data for id=2
+        es_response_2 = es.get(index="cities", id=2)
+        es_data_2 = es_response_2.get("_source", {})
+
+        # Retrieving data for id=3
+        es_response_3 = es.get(index="cities", id=3)
+        es_data_3 = es_response_3.get("_source", {})
+
+        return templates.TemplateResponse("test_elasticsearch.html", {"request": request, "es_data_1": es_data_1, "es_data_2": es_data_2, "es_data_3": es_data_3})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get('/')
 async def read_root(request: Request):
@@ -42,21 +65,20 @@ async def add_customer_db(
     companyname: str = Form(...),
     contactname: str = Form(...)
 ):
-    session = Session()
-
-    # Insérer le nouveau client dans la base de données
-    insert_query = text("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (:customerid, :companyname, :contactname)")
     try:
-        session.execute(insert_query, {"customerid": customerid, "companyname": companyname, "contactname": contactname})
-        session.commit()
+        # Index the customer information in Elasticsearch
+        es.index(index="customers", doc_type="_doc", body={
+            "customerid": customerid,
+            "companyname": companyname,
+            "contactname": contactname
+        })
         message = "Customer added successfully."
     except Exception as e:
         message = f"Error adding customer. Please check your customer information"
 
-    # Créer une réponse JSON avec le message
     response_data = {"message": message}
     return JSONResponse(content=response_data)
-    
+
 
 @app.get('/get_customer')
 async def get_customer(request: Request):
@@ -64,20 +86,19 @@ async def get_customer(request: Request):
 
 @app.get('/search_customer')
 async def search_customer(request: Request, contact_name: str):
-    session = Session()
-    result = session.execute(text('SELECT customer_id, company_name, contact_name FROM customers WHERE contact_name = :contact_name'), {"contact_name": contact_name})
-    customer = result.fetchone()
+    try:
+        # Search for customer in Elasticsearch
+        result = es.search(index="customers", body={
+            "query": {
+                "match": {
+                    "contactname": contact_name
+                }
+            }
+        })
+        customer = result['hits']['hits'][0]['_source']
 
-    if customer:
-        # Convertir les données du client en un dictionnaire JSON
-        customer_data = {
-            "customerid": customer[0],
-            "companyname": customer[1],
-            "contactname": customer[2]
-        }
-        return JSONResponse(content=customer_data)
-    else:
-        # Renvoyer une réponse JSON avec le message "Customer not found"
+        return JSONResponse(content=customer)
+    except Exception as e:
         message = {"message": f"Customer {contact_name} not found"}
         return JSONResponse(content=message)
 
